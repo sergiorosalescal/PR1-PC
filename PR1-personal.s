b46ef062f63f298c0f644029f0322a7c223148bd
sergiorosalescal <alu0101635590@ull.edu.es>
2024-02-26 19:43:20 +0000

    Modificaciones

--- PR1-personal.s
+++ PR1-personal.s
@@ -86,28 +86,65 @@ while_revision:
 #  while (tolerance < 0.00001 || tolerance >= 1) {
 while:
     li.d $f4,0.00001
-    li.d $f6,1.0 
+    l.d $f6,numero1
     c.lt.d $f20,$f4
     bc1t while_then
     c.lt.d $f20,$f6
     bc1f while_then
     b while_fin
+while_then:
 #    cout << "Error: el dato introducido debe cumplir: 0.00001 <= dato < 1" << endl;
+    li $v0,4
+    la $a0,caderr
+    syscall
 #    tolerance = 0.0;
+    li.d $f20,0.0
 #    cout << "Introduzca maximo error permitido: ";
+    li $v0,4
+    la $a0,pet
+    syscall
 #    cin >> tolerance;
     b while_revision
-    
 #  }
+while_fin:
 #  double aproximacion_numero_e{1.0}; // 1/0!
+    l.d $f26,numero1
 #  int numero_terminos{1}; 
+    l.d $f22,numero1 # pese a que sea un numero entero, lo cargaremos en double
 #  double factorial{1.0}; // 0!
+    l.d $f24,numero1
 #  while (factorial > tolerance) {
+while_generar_e:
 #    factorial /= numero_terminos++;
+    div.d $f24,$f24,$f22
+    l.d $f8,numero1
+    add.d $f22,$f22,$f8
 #    aproximacion_numero_e += factorial;
+    add.d $f26,$f26,$f24
+    c.lt.d $f24,$f20
+    bc1f while_generar_e
 #  }
 #  cout << "Numero e: " << fixed << setprecision(17) << aproximacion_numero_e << endl;
+    li $v0,4
+    la $a0,cade
+    syscall
+    li $v0,3
+    mov.d $f12,$f26
+    syscall
 #  cout << "Numero de terminos: " << numero_terminos << endl;
+    li $v0,4
+    la $a0,cadnt
+    syscall
+    cvt.w.d $f10,$f22
+    mfc1 $s0,$f10
+    li $v0,1
+    move $a0,$s0
+    syscall
 #  cout << "Fin del programa." << endl;
-#  return 0;
+    li $v0,4
+    la $a0,cadfin
+    syscall
+#   return 0;
+    li$v0,10
+    syscall
 #}
